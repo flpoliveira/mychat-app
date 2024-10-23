194b544b699d4f6a0bd6d8b0ef7b62989dce960a
Filipe Oliveira <filipedasilvadeoliveira@gmail.com>
2024-10-22 21:40:47 -0300

    fix: change implementation using lodash

--- app/helpers/buildMessagesPerDay.ts
+++ app/helpers/buildMessagesPerDay.ts
@@ -1,32 +1,24 @@
 import { UserMessageType } from "@/context/chat.interface";
 import { getDayLabel } from "./getDayLabel";
+import lodash from "lodash";
 
 function buildMessagesPerDay(
   messages: UserMessageType[]
 ): Array<{ type?: "date" } & UserMessageType> {
-  const messagesPerDay = {} as Record<string, UserMessageType[]>;
-
-  messages.forEach((message) => {
-    const date = new Date(message.timestamp).toDateString();
-
-    if (!messagesPerDay[date]) {
-      messagesPerDay[date] = [];
-    }
-
-    messagesPerDay[date].push(message);
+  const groupedMessages = lodash.groupBy(messages, (message) => {
+    return new Date(message.timestamp).toDateString();
   });
 
-  const days = Object.keys(messagesPerDay).sort((a, b) => {
+  const sortedDates = Object.keys(groupedMessages).sort((a, b) => {
     return new Date(b).getTime() - new Date(a).getTime();
   });
 
-  return days.reduce((acc, key) => {
+  return lodash.flatMap(sortedDates, (date) => {
     return [
-      ...acc,
-      { type: "date", content: getDayLabel(key) },
-      ...messagesPerDay[key],
+      { type: "date", content: getDayLabel(date) },
+      ...groupedMessages[date],
     ] as Array<{ type: "date" } & UserMessageType>;
-  }, [] as Array<{ type: "date" } & UserMessageType>);
+  });
 }
 
 export { buildMessagesPerDay };
